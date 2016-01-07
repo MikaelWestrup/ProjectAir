d3c15c2ff21fe0c63bcd3976cbfda6072dd514f7
Ankit Patidar <ankit.p@opensoftindia.com>
2016-01-07 17:53:39 +0530

    js modified

--- app/assets/javascripts/angular-js/app.js
+++
@@ -1,5 +0,0 @@
-var myApp = angular.module('myApp', ['ngResource']);
-
-myApp.config(function($httpProvider) {
-  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-});
