a6633e64a7a17fcdcddc90cdc7a03beaab70d732
MikaelWestrup <mikael.westrup@focused.se>
2016-05-16 16:10:46 +0200

    Comment last change
    Comment last change

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,6 @@ module AuditPlanning
 end
 
 # Prevent initializing the application before assets are precompiled (required for heroku) 
-config.assets.initialize_on_precompile = false 
+#config.assets.initialize_on_precompile = false 
 # Add Rails Admin assets (required) 
-config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+#config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
